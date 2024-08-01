aa9a56feddd9fcd4fb19337f7091ca1efbbf9bd0
João Salles <110571720+salles18@users.noreply.github.com>
2024-08-01 11:09:59 -0300

    Update treemap_population.py

--- treemap_population.py
+++ treemap_population.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import plotly.express as px
 
-# Dados fictícios de população de países
+
 data = {
     'Country': ['China', 'India', 'United States', 'Indonesia', 'Pakistan', 'Brazil', 'Nigeria', 'Bangladesh', 'Russia', 'Mexico'],
     'Population': [1444216107, 1393409038, 332915073, 276361783, 225199937, 213993437, 211400708, 166303498, 145912025, 130262216],
     'Area': [9600000, 3287000, 9370000, 1900000, 881000, 8516000, 923768, 147570, 17098242, 1964375]  # Área em km²
 }
 
-# Criar um DataFrame
+
 df = pd.DataFrame(data)
 
-# Criar o treemap
+
 fig = px.treemap(df, 
                  path=['Country'], 
                  values='Population',
@@ -20,13 +20,13 @@ fig = px.treemap(df,
                  color_continuous_scale='Viridis',
                  hover_data={'Country': True, 'Population': True, 'Area': True})
 
-# Adicionar informações detalhadas ao passar o mouse
+
 fig.update_traces(
     hovertemplate="<b>%{label}</b><br>Population: %{value:,}<br>Area: %{customdata[1]:,} km²<extra></extra>"
 )
 
-# Salvar o treemap como um arquivo HTML
+
 fig.write_html("treemap_population_interactive.html")
 
-# Exibir o treemap
+
 fig.show()
